bda7cab89c48f28c06b4b7edf4f2c8fb328fd63b
LEE <1528315273@qq.com>
2019-07-23 12:21:48 +0800

    add study

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/jony-lee/go-way/study"
 )
 
 func main() {
+	study.Test()
 	fmt.Println("good")
 }
---
+++ study/day001.go
@@ -0,0 +1,7 @@
+package study
+
+import "fmt"
+
+func Test() {
+	fmt.Println("just for test!")
+}
