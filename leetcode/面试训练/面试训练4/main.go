54e616b02b45da2e3d31c1dbf985b8ccdcda9798
jony-lee <jonylee.cn@gmail.com>
2019-08-10 09:53:45 +0800

    1

--- leetcode/面试训练/面试训练4/main.go
+++ leetcode/面试训练/面试训练4/main.go
@@ -67,19 +67,22 @@ func isMatch(s string, p string) bool {
 	if len(s) != 0 && len(p) == 0 { //字符串不为空，匹配为空则失败
 		return false
 	}
+
 	j := 0
 	for i := 0; i < len(s); i++ {
-		if s[i] == p[j] || p[j] == '?' {
-			j++
+		if s[i] !=p[j]&&p[j]!='?'&&p[j]!='*'{
+			return false
 		}
-		if j == len(p)-1 && p[j] == '*' {
-			return true
-		}
-		if p[j] == '*' && p[j+1] != s[i+1] {
+
+		if s[i]==p[j]||p[j]=='?'{
 			j++
 			continue
+		}else if p[j]=='*'{
+			continue
 		}
+
 	}
+	return true
 }
 
 //【主函数】
@@ -116,3 +119,43 @@ func main() {
 
 
 */
+boolean isMatch(String str, String pattern) {
+int s = 0, p = 0, match = 0, starIdx = -1;
+//遍历整个字符串
+while (s < str.length()){
+// 一对一匹配，两指针同时后移。
+if (p < pattern.length()  && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))){
+s++;
+p++;
+}
+// 碰到 *，假设它匹配空串，并且用 startIdx 记录 * 的位置，记录当前字符串的位置，p 后移
+else if (p < pattern.length() && pattern.charAt(p) == '*'){
+starIdx = p;
+match = s;
+p++;
+}
+// 当前字符不匹配，并且也没有 *，回退
+// p 回到 * 的下一个位置
+// match 更新到下一个位置
+// s 回到更新后的 match
+// 这步代表用 * 匹配了一个字符
+else if (starIdx != -1){
+p = starIdx + 1;
+match++;
+s = match;
+}
+//字符不匹配，也没有 *，返回 false
+else return false;
+}
+
+//将末尾多余的 * 直接匹配空串 例如 text = ab, pattern = a*******
+while (p < pattern.length() && pattern.charAt(p) == '*')
+p++;
+
+return p == pattern.length();
+}
+
+作者：windliang
+链接：https://leetcode-cn.com/problems/two-sum/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-9/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
